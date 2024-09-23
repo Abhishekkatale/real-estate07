c7604d88be812e9096bf92863dd0fb29e2dfee2a
Abhishekkatale <110841864+Abhishekkatale@users.noreply.github.com>
2024-09-23 18:21:25 +0530

    Update OurTeam.jsx

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ServiceCard from '../components/ServiceCard';
+import OurLocation from '../components/OurLocation';
+
 
 
 const OurTeam = () => (
   <>
     <Navbar />
-    
+    <ServiceCard/>
+    <OurLocation/>
     <Footer />
   </>
 );
